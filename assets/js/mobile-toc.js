c891c5ce63ff59405b2c97535f708911b933fef2
🛡️ Jadson Lima <h4ckthreat@gmail.com>
2026-06-10 14:53:51 -0400

    Update mobile-toc.js

--- assets/js/mobile-toc.js
+++ assets/js/mobile-toc.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+  const toc = document.querySelector("#toc");
+
+  if (!toc) return;
+
+  document.getElementById("mobile-toc-content").innerHTML = toc.innerHTML;
+
+  document
+    .getElementById("mobile-toc-btn")
+    .addEventListener("click", () => {
+      document
+        .getElementById("mobile-toc-modal")
+        .classList.add("show");
+    });
+
+  document
+    .getElementById("mobile-toc-close")
+    .addEventListener("click", () => {
+      document
+        .getElementById("mobile-toc-modal")
+        .classList.remove("show");
+    });
+
+});
