339f9e469377bec3ab15464c82395e8afed50a75
🛡️ Jadson Lima <h4ckthreat@gmail.com>
2026-06-11 10:51:39 -0400

    Add files via upload

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,17 +1,31 @@
-function initToc() {
-  const content = document.querySelector(".post-content");
-  const toc = document.querySelector("#toc");
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.querySelector(".post-content")) {
+    tocbot.init({
+      tocSelector: "#toc",
+      contentSelector: ".post-content",
+      ignoreSelector: "[data-toc-skip]",
+      headingSelector: "h2, h3",
+      orderedList: false,
+      scrollSmooth: false
+    });
 
-  if (!content || !toc) return;
+    // copia o TOC do desktop para o TOC do celular
+    const toc = document.querySelector("#toc");
+    const mobileToc = document.querySelector("#mobile-toc");
 
-  tocbot.init({
-    tocSelector: "#toc",
-    contentSelector: ".post-content",
-    ignoreSelector: "[data-toc-skip]",
-    headingSelector: "h2, h3",
-    orderedList: false,
-    scrollSmooth: false
+    if (toc && mobileToc) {
+      setTimeout(() => {
+        mobileToc.innerHTML = toc.innerHTML;
+      }, 100);
+    }
+  }
+});
+
+const toggle = document.querySelector(".mobile-toc-toggle");
+const links = document.querySelector(".mobile-toc-links");
+
+if (toggle && links) {
+  toggle.addEventListener("click", () => {
+    links.classList.toggle("open");
   });
 }
-
-document.addEventListener("DOMContentLoaded", initToc);
